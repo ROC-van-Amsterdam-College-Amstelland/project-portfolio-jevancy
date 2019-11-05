be485bb39aa10971c47244578234789ae285d5c1
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-11-05 10:40:43 +0100

    plaatjes klaar

---
+++ script.js
@@ -0,0 +1,8 @@
+function verander() {
+    //hide foto 1
+    document.getElementById("foto1").style.display= "none";
+    //show foto 2
+    document.getElementById("foto2").style.display="block";
+}
+
+//en dan voor elke foto
